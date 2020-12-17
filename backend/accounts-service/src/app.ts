42f6c75ecacb95e328e19c031d1301dfeccb4ea1
rafael <rafortes987@gmail.com>
2020-12-17 10:31:13 -0300

    first commit

---
+++ backend/accounts-service/src/app.ts
@@ -0,0 +1,14 @@
+import exprees from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import accountsRouter from './routes/accounts';
+
+const app = exprees();
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(accountsRouter);
+
+const port = parseInt(`${process.env.PORT}`);
+
+app.listen(3000);
+console.log(`Running on port ${port}`);
